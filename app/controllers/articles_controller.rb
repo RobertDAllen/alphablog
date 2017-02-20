1c46622cb62138d19cd24a09c83bfdd3062cb963
Robert Rails Playtest <contact@robertdallen.com>
2017-02-20 21:40:35 +0000

    Set all edit and update functionality, index listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,20 @@
 class ArticlesController < ApplicationController
+   def index
+    @articles = Article.all
+   end
+   
+   
    def new
        @article = Article.new
    end
    
-   def create
+    def edit
+    @article = Article.find(params[:id])
+   
+    end
+   
+   
+    def create
        @article = Article.new(article_params)
         if @article.save
             flash[:notice] = "Article was successfully created"
@@ -11,16 +22,27 @@ class ArticlesController < ApplicationController
         else
             render :new
         end
-   end
+    end
    
-   def show
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was updated"
+            redirect_to article_path(@article)
+        else
+            render :edit
+        end
+    end
+   
+    def show
        @article = Article.find(params[:id])
-   end
+    end
     
     private
         def article_params
             params.require(:article).permit(:title, :description)
         end
     
+
     
 end
