4a33b0b822ada0d436c04fb602514d56f84e9d18
Robert Rails Playtest <contact@robertdallen.com>
2017-03-01 22:35:27 +0000

    Fixed bug with articles partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,29 @@
 class UsersController < ApplicationController
-   def new
+   
+   def index
+        @users = User.all
+   end
+   
+   
+   
+   
+    def new
        @user = User.new
-   end
-   
-   def create
-    @user = User.new(user_params)
-    if @user.save
-        flash[:success] = "Welcome, #{@user.username}!"
-        redirect_to articles_path
-    else
-        render 'new'
     end
-   end
    
-   def edit
-      @user = User.find(params[:id]) 
-   end
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            flash[:success] = "Welcome, #{@user.username}!"
+            redirect_to articles_path
+        else
+            render 'new'
+        end
+    end
+   
+    def edit
+        @user = User.find(params[:id]) 
+    end
    
     def update
         @user = User.find(params[:id]) 
@@ -35,7 +43,7 @@ class UsersController < ApplicationController
    
    private
    def user_params
-    params.require(:user).permit(:username, :email, :password)
+       params.require(:user).permit(:username, :email, :password)
    end
    
    
